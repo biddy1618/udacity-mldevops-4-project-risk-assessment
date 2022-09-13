0f91233d7bfd07f81b18a489fcbacb408a3ede49
Dauren <dauren.baitursyn@toptal.com>
2022-09-13 07:40:17 +0600

    Minor - linting fixes

--- fullprocess.py
+++ fullprocess.py
@@ -7,16 +7,16 @@ Date: 13.09.22
 import sys
 import json
 import logging
-import pandas as pd
+# import pandas as pd
 
 from pathlib import Path
 from ast import literal_eval
 
-import training
-import scoring
-import deployment
-import diagnostics
-import reporting
+# import training
+# import scoring
+# import deployment
+# import diagnostics
+# import reporting
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logger = logging.getLogger()
@@ -27,35 +27,37 @@ with open('config.json', 'r') as f:
 input_folder_path = Path.joinpath(Path.cwd(), config['input_folder_path'])
 output_folder_path = Path.joinpath(Path.cwd(), config['output_folder_path'])
 
-##################Check and read new data
-#first, read ingestedfiles.txt
+# Check and read new data
+# first, read ingestedfiles.txt
 with open(Path.joinpath(output_folder_path, 'ingestedfiles.txt'), 'r') as f:
     ingested_files = literal_eval(f.read())
 
-#second, determine whether the source data folder has files that aren't listed in ingestedfiles.txt
+# second, determine whether the source data folder has files that aren't
+# listed in ingestedfiles.txt
 source_files = list(f.name for f in input_folder_path.iterdir())
 new_files = False
 if not set(ingested_files).issuperset(source_files):
     new_files = True
 
-##################Deciding whether to proceed, part 1
-#if you found new data, you should proceed. otherwise, do end the process here
+# Deciding whether to proceed, part 1
+# if you found new data, you should proceed. otherwise, do end the process here
 
 
-##################Checking for model drift
-#check whether the score from the deployed model is different from the score from the model that uses the newest ingested data
+# Checking for model drift
+# check whether the score from the deployed model is different from the score
+# from the model that uses the newest ingested data
 
 
-##################Deciding whether to proceed, part 2
-#if you found model drift, you should proceed. otherwise, do end the process here
+# Deciding whether to proceed, part 2
+# if you found model drift, you should proceed. otherwise, do end the process 
+# here
 
 
+# Re-deployment
+# if you found evidence for model drift, re-run the deployment.py script
 
-##################Re-deployment
-#if you found evidence for model drift, re-run the deployment.py script
-
-##################Diagnostics and reporting
-#run diagnostics.py and reporting.py for the re-deployed model
+# Diagnostics and reporting
+# run diagnostics.py and reporting.py for the re-deployed model
 
 
 
